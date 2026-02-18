47c3d90a6ee477381a7e2545cb9ece78b0253331
Diegesis & Mimesis <diegesisandmimesis@proton.me>
2026-02-17 19:05:55 -0800

    Sync

--- calendar.h
+++ calendar.h
@@ -3,16 +3,6 @@
 //
 
 #ifdef CALENDAR_EVENTS
-/*
-#include "eventHandler.h"
-#ifndef EVENT_HANDLER_H
-#error "This module requires the eventHandler module."
-#error "https://github.com/diegesisandmimesis/eventHandler"
-#error "It should be in the same parent directory as this module.  So if"
-#error "calendar is in /home/user/tads/calendar, then"
-#error "eventHandler should be in /home/user/tads/eventHandler ."
-#endif // EVENT_HANDLER_H
-*/
 #include "asyncEvent.h"
 #ifndef ASYNC_EVENT_H
 #error "This module requires the asyncEvent module."
